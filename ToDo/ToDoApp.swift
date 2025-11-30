c317475d16be47ac58a4218346a707fd0c01a807
Abhishek Raut <abhraut@linkedin.com>
2025-11-30 19:20:35 +0530

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Abhishek Raut on 30/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
